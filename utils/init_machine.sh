9feca5c3158aff682fe02bdd18b098a84b66ef83
Wei Qiu <qiuw@student.ethz.ch>
2021-12-31 17:03:38 +0800

    add plot function time util

---
+++ utils/init_machine.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#eg: init_machine
+machine=$1
+mkdir /root/usm_plot_data_needed/$machine
+mkdir /root/usm_plot_data_needed/$machine/time_cost
+mkdir /root/usm_plot_data_needed/$machine/memory_usage
+mkdir /root/usm_plot_data_needed/$machine/sharing_potential
+mkdir /root/usm_plot_data_needed/$machine/time_cost/image-recognition
+mkdir /root/usm_plot_data_needed/$machine/time_cost/micro-benchmark
